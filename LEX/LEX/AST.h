9e9375be7debfb87868b87b7d73a5a7364faaa3e
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-11-30 15:50:16 +0900

    意味解析器の制御用列挙クラスの作成 ノードの削除 構文木作成の下準備(仮)

---
+++ LEX/LEX/AST.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "LEX.h"
+
+//==============================
+//ノード列挙クラス
+//==============================
+enum class NODE {
+	Null,		//
+	STRING,		//
+	NUMBER,		//
+	SPAR,		//
+	EPAR,		//
+	SBRACKET,	//
+	EBRACKET,	//
+	VBAR,		//
+	DOLL,		//
+	VARIABLE,	//
+	SHARP,		//
+	TAB,		//
+	OP,			// 演算子ノード
+};
+
+//==============================
+//抽象構文木の基礎クラス
+//==============================
+class AST {
+private:
+protected:
+public:
+	virtual NODE GetNodeType() = 0;
+};
+
+//=================================
+//数値ノード
+//=================================
+class NODE_NUMBER:public AST {
+public:
+	int value;
+	virtual NODE GetNodeType() {
+		return NODE::NUMBER;
+	}
+
+	NODE_NUMBER(int v):value(v){}	//初期化処理
+};
+
+//=================================
+//変数ノード
+//=================================
+class NODE_VARIABLE :public AST {
+public:
+	string varName;		//変数名
+	virtual NODE GetNodeType() {
+		return NODE::VARIABLE;
+	}
+};
+
+//=================================
+//演算子ノード
+//=================================
+class NODE_OP :public AST {
+public:
+	string op;		//演算子
+	AST *left;		//左辺のノード
+	AST *right;		//右辺のノード
+	virtual NODE GetNodeType() { return NODE::OP; }
+
+	NODE_OP(string _op, AST *_left, AST *_right) :
+		op(_op), left(_left), right(_right) {}
+};
+
+//====================================
+//関数ノード
+//====================================
--- LEX/LEX/SEMANTIC.h
+++ LEX/LEX/SEMANTIC.h
@@ -1,9 +1,20 @@
 #pragma once
 #include "PARSE.h"
+
+enum class OP_CODE {
+	PUSH,		//プッシュ
+	ADD,		//加算
+	SUB,		//減算
+	MUL,		//乗算
+	DIV,		//除算
+				//余り
+				//代入
+	TEXT,		//テキスト表示
+	FUNC		//関数実行
+};
+
 class SEMANTIC {
 private:
-	NODE* root;		//構文解析で作成した構文木のルートを取得
-	NODE* EndNode();		//末端ノードを取得
 
 public:
 };
