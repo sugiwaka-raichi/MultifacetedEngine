135614233a4855f8c0fbd99afb0577d100c3275a
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-12-08 16:39:28 +0900

    レイヤーと表示非表示の管理を追加

--- MultifacetedEngine/Sorces/OBJECT.cpp
+++ MultifacetedEngine/Sorces/OBJECT.cpp
@@ -1,1 +1,18 @@
 #include "OBJECT.h"
+
+void OBJECT::SetLayernum(int _layer) {
+	layer = _layer;
+}
+
+void OBJECT::SetDispFlag(bool _flag) {
+	dispFlag = _flag;
+}
+
+int OBJECT::GetLayernum() {
+	return layer;
+}
+
+bool OBJECT::GetDispFlag()
+{
+	return dispFlag;
+}
--- MultifacetedEngine/Sorces/OBJECT.h
+++ MultifacetedEngine/Sorces/OBJECT.h
@@ -5,12 +5,18 @@
 class OBJECT {
 private:
 protected:
-	int layer;		//レイヤー　表示順決定に使う
-	bool show_flag;	//表示フラグ
+	int layer = 0;			//レイヤー　表示順決定に使う
+	bool dispFlag = true;	//表示フラグ
 public:
 	virtual void Init() = 0;		//初期化純粋仮想関数
 	virtual void Update() = 0;		//更新純粋仮想関数
 	virtual void Draw() = 0;		//描画純粋仮想関数
 	virtual void UnInit() = 0;		//解放純粋仮想関数
+	
+	void SetLayernum(int _layer);
+	void SetDispFlag(bool _flag);
+	int GetLayernum();				//レイヤー番号取得
+	bool GetDispFlag();				//表示フラグ取得
+
 };
 
