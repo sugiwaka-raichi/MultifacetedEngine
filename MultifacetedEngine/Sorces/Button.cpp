e91e39cf49b8c825272df5872d0cb046e817bd6d
sugiwaka-raichi <72175300+sugiwaka-raichi@users.noreply.github.com>
2020-12-08 16:38:36 +0900

    Press→Triggerに変更

--- MultifacetedEngine/Sorces/Button.cpp
+++ MultifacetedEngine/Sorces/Button.cpp
@@ -5,27 +5,27 @@ bool Button::OnClick(MOUSE_KEYTYPE m_key) {
 	if (OnCursor()) {
 		switch (m_key) {
 		case MOUSE_KEYTYPE::LEFT:
-			if (GetMousePress(MOUSE_KEYTYPE::LEFT)){
+			if (GetMouseTrigger(MOUSE_KEYTYPE::LEFT)){
 				return true;
 			}
 			break;
 		case MOUSE_KEYTYPE::RIGHT:
-			if (GetMousePress(MOUSE_KEYTYPE::RIGHT)) {
+			if (GetMouseTrigger(MOUSE_KEYTYPE::RIGHT)) {
 				return true;
 			}
 			break;
 		case MOUSE_KEYTYPE::CENTER:
-			if (GetMousePress(MOUSE_KEYTYPE::CENTER)) {
+			if (GetMouseTrigger(MOUSE_KEYTYPE::CENTER)) {
 				return true;
 			}
 			break;
 		case MOUSE_KEYTYPE::FORWARD:
-			if (GetMousePress(MOUSE_KEYTYPE::FORWARD)) {
+			if (GetMouseTrigger(MOUSE_KEYTYPE::FORWARD)) {
 				return true;
 			}
 			break;
 		case MOUSE_KEYTYPE::BACK:
-			if (GetMousePress(MOUSE_KEYTYPE::BACK)) {
+			if (GetMouseTrigger(MOUSE_KEYTYPE::BACK)) {
 				return true;
 			}
 			break;
